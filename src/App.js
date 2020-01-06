6034343965f3b2afd1cbbfe925b00c4b58667ce1
Timothé Duc <timotheduc@gmail.com>
2020-01-06 17:59:38 +0100

    first commit on front end

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>Wiibus front end with FeathersJS API</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
